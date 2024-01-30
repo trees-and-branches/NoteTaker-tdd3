af59db94a2487bfd2ec8d687d3fef0dab6ceb211
spoons <ev4n.mus1c@gmail.com>
2024-01-30 16:12:09 -0700

    Initial Commit

---
+++ NoteTaker tdd3/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NoteTaker tdd3
+//
+//  Created by shark boy on 1/30/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NoteTaker tdd3/NoteTaker_tdd3App.swift
@@ -0,0 +1,17 @@
+//
+//  NoteTaker_tdd3App.swift
+//  NoteTaker tdd3
+//
+//  Created by shark boy on 1/30/24.
+//
+
+import SwiftUI
+
+@main
+struct NoteTaker_tdd3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
